637bee0976aebe555593406c0bde5bc2b2026e1d
SMART24\User <fds44fs@nate.com>
2025-05-19 14:40:40 +0900

    dudu

--- lib/singnup_page.dart
+++ lib/singnup_page.dart
@@ -1,53 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({super.key});
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
 
   @override
-  State<LoginPage> createState() => _LoginPageState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignUpPageState extends State<SignUpPage> {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController pwController = TextEditingController();
   final SupabaseClient supabase = Supabase.instance.client;
 
-  Future<void> _login() async{
+  Future<void> _signup() async{
     final email = emailController.text.trim();
     final pw = pwController.text.trim();
 
     try {
-      final response = await supabase.auth.signInWithPassword(
+      await supabase.auth.signUp(
           email: email,
           password: pw
       );
 
-      if (response.user != null) {
-        Navigator.pushReplacementNamed(context, '/todo-remote');
-      }
-    } catch (e){
-      try {
-        await supabase.auth.signUp(email: email, password: pw);
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("회원가입 성공! \n 이메일을 인증을 진행한 뒤에 다시 로그인 해보세요."))
+      );
 
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("회원가입 성공! 다시 로그인 해보세요."))
-        );
-      } catch (e){
-        ScaffoldMessenger.of(context).showSnackBar(
-            SnackBar(content: Text("회원가입 실패: ${e}"))
-        );
-      }
-      /*ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("로그인 실패: ${e}"))
-      );*/
+      Navigator.pushReplacementNamed(context, '/login');
+    } catch (e){
+      ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text("회원가입 실패: ${e}"))
+      );
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("로그인")),
+      appBar: AppBar(title: const Text("회원가입")),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
@@ -58,13 +50,14 @@ class _LoginPageState extends State<LoginPage> {
             ),
             const SizedBox(height: 12),
             TextField(
+              obscureText: true,
               controller: pwController,
               decoration: const InputDecoration(labelText: "비밀번호"),
             ),
             const SizedBox(height: 12),
             ElevatedButton(
-              onPressed: _login,
-              child: const Text("로그인"),
+              onPressed: _signup,
+              child: const Text("회원가입"),
             )
           ],
         ),
